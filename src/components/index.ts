a964c2a97b27682c6262eca1e90c98af4c402cd7
Aleksandr Mischenko <h-alex@bk.ru>
2021-02-17 14:10:10 +0300

    fix webpack warnings

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,29 @@
 export {default as Input} from './Input';
+export type {InputProps} from './Input';
+
 export {default as InputUnit} from './InputUnit';
+export type {InputUnitProps} from './InputUnit';
+
 export {default as BodyPortal} from './BodyPortal';
+export type {BodyPortalProps} from './BodyPortal';
+
 export {default as Button} from './Button';
+export type {ButtonProps} from './Button';
+
 export {default as ButtonGroup} from './ButtonGroup';
+export type {ButtonGroupProps} from './ButtonGroup';
+
 export {default as LoaderDots} from './LoaderDots';
+export type {LoaderProps} from './LoaderDots';
+
 export {default as Dropdown} from './Dropdown';
+export type {DropdownProps} from './Dropdown';
+
 export {default as Checkbox} from './Checkbox';
+// export type {CheckboxProps} from './Checkbox';
+
 export {default as Radio} from './Radio';
+// export type {RadioProps} from './Radio'; // TODO: refactor
+
 export {default as Link} from './Link';
+export type {LinkProps} from './Link';
